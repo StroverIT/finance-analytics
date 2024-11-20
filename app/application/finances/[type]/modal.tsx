510a520456fe2311f9cde3ba48a78bc8dbd1f5e4
StroverIT <xcreaw1@abv.bg>
2024-11-20 15:29:34 +0200

    Uploading image

--- app/application/finances/[type]/modal.tsx
+++ app/application/finances/[type]/modal.tsx
@@ -1,12 +1,16 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import HeaderWithBackBtn from "@/components/Atoms/HeaderWithBackBtn";
 import Input from "@/components/Atoms/Input";
 import Button from "@/components/Atoms/Button";
+import * as ImagePicker from "expo-image-picker";
+import { ColorThemes } from "@/components/Atoms/Button/types";
 
 const Modal = () => {
   const [value, setValue] = useState("");
+  const [image, setImage] = useState<string | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   const { type } = useLocalSearchParams();
 
@@ -22,6 +26,48 @@ const Modal = () => {
 
   const onChange = (text: string) => setValue(text);
   const submit = () => {};
+  const pickImage = async () => {
+    try {
+      // No permissions request is necessary for launching the image library
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ["images", "videos"],
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      // @ts-ignore
+
+      const formData = new FormData();
+
+      // @ts-ignore
+      const file = result.assets[0];
+
+      // @ts-ignore
+      formData.append("image", {
+        uri: file.uri,
+        type: file.mimeType,
+        name: file.fileName,
+      });
+
+      const options = {
+        method: "POST",
+        body: formData,
+      };
+
+      const res = await fetch(
+        `${process.env.EXPO_PUBLIC_SERVER_IP}/uploadImages`,
+        options
+      );
+      const { imageUrl, message } = await res.json();
+      console.log("test+++", imageUrl, message);
+      if (message) {
+        setImageUrl(imageUrl);
+        setImage(imageUrl);
+      }
+    } catch (e) {
+      console.log("test+++ error", e);
+    }
+  };
 
   return (
     <SafeAreaView className="mx-4 mt-4">
@@ -34,7 +80,15 @@ const Modal = () => {
           label={typeName || ""}
         />
         <View className="mt-4">
-          <Button text="Добави" onPress={submit} />
+          <Button
+            text="Избери снимка от галерията"
+            theme={ColorThemes.green}
+            onPress={pickImage}
+          />
+        </View>
+        {image && <Image source={{ uri: image }} className="h-96 w-full" />}
+        <View className="mt-8">
+          <Button text={`Създай ${typeName}`} onPress={submit} />
         </View>
       </View>
     </SafeAreaView>

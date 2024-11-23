0ca94c0df5ab9db5126cb966ebcd93b15f39efb9
StroverIT <xcreaw1@abv.bg>
2024-11-23 14:35:21 +0200

    Changing splashscreen to logo Changing logo Changing name from frontend to WalletWay

--- components/Molecules/LogoWithName/LogoWithName.tsx
+++ components/Molecules/LogoWithName/LogoWithName.tsx
@@ -6,7 +6,7 @@ export const LogoWithName: FC<LogoWithNameProps> = () => {
   return (
     <View className="flex items-center justify-center flex-row">
       <Image
-        source={require("@/assets/images/logo.png")}
+        source={require("@/assets/images/icon-v2-nobg.png")}
         className="h-24 w-24"
       />
 
